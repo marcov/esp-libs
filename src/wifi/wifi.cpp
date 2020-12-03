bb2c28717fd32ea02dfd557644c2ebe22042b123
Marco Vedovati <mv@sba.lat>
2020-12-03 10:57:55 +0100

    wifi: dirty fixes

--- src/wifi/wifi.cpp
+++ src/wifi/wifi.cpp
@@ -3,13 +3,10 @@
 
 #include "wifi.h"
 
-static const char* wifi_ssid;
-static const char* wifi_password;
-
-static bool was_connected;
-
 bool wifiHasConnected(void)
 {
+    static bool was_connected = false;
+
     bool now_connected = WiFi.status() == WL_CONNECTED;
 
     if (!was_connected && now_connected)
@@ -23,18 +20,32 @@ bool wifiHasConnected(void)
     else if (was_connected && !now_connected)
     {
         Serial.println("WiFi now disconnected");
-        // try reconnect...
-        WiFi.begin(wifi_ssid, wifi_password);
-
         was_connected = WiFi.status() == WL_CONNECTED;
     }
 
     return false;
 }
 
+static inline uint32_t waitForWifiConnected(void)
+{
+    uint32_t elapsedMs;
+    constexpr uint32_t sleepMs = 500;
+
+    elapsedMs = 0;
+
+    while (!wifiHasConnected())
+    {
+        delay(sleepMs);
+        elapsedMs += sleepMs;
+        Serial.print(".");
+    }
+
+    return elapsedMs;
+}
+
 void wifiSetup(bool mode_ap, bool allowSleep, const char *ssid, const char *password)
 {
-    was_connected = false;
+    WiFi.persistent(false);
 
     WiFi.disconnect();
 
@@ -43,14 +54,19 @@ void wifiSetup(bool mode_ap, bool allowSleep, const char *ssid, const char *pass
                    " ssid: " + ssid +
                    " password: " + password);
 
-    if (mode_ap) {
+    if (mode_ap)
+    {
         WiFi.mode(WIFI_AP);
         WiFi.softAP(ssid, password);
-    } else {
+    }
+    else
+    {
         WiFi.mode(WIFI_STA);
         WiFi.begin(ssid, password);
         WiFi.setAutoReconnect(true);
-    }
 
-    WiFi.setSleepMode(allowSleep ? WIFI_LIGHT_SLEEP : WIFI_NONE_SLEEP);
+        waitForWifiConnected();
+
+        WiFi.setSleepMode(allowSleep ? WIFI_LIGHT_SLEEP : WIFI_NONE_SLEEP);
+    }
 }
