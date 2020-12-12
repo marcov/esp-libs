645915896085ef11630b3394bf7fe054827eddfe
Marco Vedovati <marco@Marcos-Air.lan>
2020-12-12 19:31:42 +0100

    update wifi mgmt

--- src/wifi/wifi.cpp
+++ src/wifi/wifi.cpp
@@ -1,56 +1,164 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
 
 #include "wifi.h"
 
-static const char* wifi_ssid;
-static const char* wifi_password;
+#define ENABLE_DEBUG 0
 
-static bool was_connected;
+#if ENABLE_DEBUG
+#  define DEBUG(str)  Serial.println(str)
+#else
+#  define DEBUG(str)  do {/**/} while (0)
+#endif
 
-bool wifiHasConnected(void)
+/*!
+
+   @brief WIFI ctor
+
+ */
+WIFI::WIFI (
+    bool ModeAP,
+    bool AllowSleep,
+    const char *Ssid,
+    const char *Password,
+    uint32_t ConnectTimeoutMs
+    ) :
+    m_ModeAP(ModeAP),
+    m_AllowSleep(AllowSleep),
+    m_Ssid(Ssid),
+    m_Password(Password),
+    m_ConnectTimeoutMs(ConnectTimeoutMs)
 {
-    bool now_connected = WiFi.status() == WL_CONNECTED;
-
-    if (!was_connected && now_connected)
-    {
-        Serial.print("WiFi now connected, IP: ");
-        Serial.println(WiFi.localIP());
-
-        was_connected = now_connected;
-        return true;
-    }
-    else if (was_connected && !now_connected)
-    {
-        Serial.println("WiFi now disconnected");
-        // try reconnect...
-        WiFi.begin(wifi_ssid, wifi_password);
-
-        was_connected = WiFi.status() == WL_CONNECTED;
-    }
-
-    return false;
+    WiFi.persistent(false);
+    m_State = DISCONNECTED;
 }
 
-void wifiSetup(bool mode_ap, bool allowSleep, const char *ssid, const char *password)
+/*!
+
+   @brief Wait until WiFi is connected
+
+   @param TimeoutMs The max timeout in ms to wait. If 0, wait forever.
+
+   @return 0 on success, 1 if timeout expired
+
+ */
+int
+WIFI::WaitForConnection (
+    unsigned long TimeoutMs
+    )
 {
-    was_connected = false;
+    uint32_t elapsedMs;
+    constexpr uint32_t pollPeriodMs = 500;
 
-    WiFi.disconnect();
-
-    Serial.println("WiFi start mode: " +
-                   String(mode_ap ? "AP" : "STA") +
-                   " ssid: " + ssid +
-                   " password: " + password);
-
-    if (mode_ap) {
-        WiFi.mode(WIFI_AP);
-        WiFi.softAP(ssid, password);
-    } else {
-        WiFi.mode(WIFI_STA);
-        WiFi.begin(ssid, password);
-        WiFi.setAutoReconnect(true);
+    for(elapsedMs = 0;
+        !WIFI::IsConnected() && (TimeoutMs == WIFI::WAIT_FOREVER || elapsedMs < TimeoutMs);
+        elapsedMs += pollPeriodMs)
+    {
+        delay(pollPeriodMs);
+        elapsedMs += pollPeriodMs;
+        Serial.print(".");
     }
 
-    WiFi.setSleepMode(allowSleep ? WIFI_LIGHT_SLEEP : WIFI_NONE_SLEEP);
+    if (elapsedMs < TimeoutMs)
+    {
+        m_State = CONNECTED;
+        return 0;
+    }
+
+    return 1;
+}
+
+void
+WIFI::Connect (
+    void
+    )
+{
+    Connect(m_ConnectTimeoutMs);
+}
+
+void
+WIFI::Connect (
+    const unsigned long ConnectTimeoutMs
+    )
+{
+    WiFi.disconnect();
+    Serial.println("WiFi start mode: " +
+                   String(m_ModeAP ? "AP" : "STA") +
+                   " ssid: " + m_Ssid +
+                   " password: " + m_Password);
+
+    if (m_ModeAP)
+    {
+        WiFi.mode(WIFI_AP);
+        WiFi.softAP(m_Ssid, m_Password);
+    }
+    else
+    {
+        WiFi.mode(WIFI_STA);
+        WiFi.begin(m_Ssid, m_Password);
+        WiFi.setAutoReconnect(true);
+
+        m_State = CONNECTING;
+        WIFI::WaitForConnection(ConnectTimeoutMs);
+
+        if (m_State != CONNECTED)
+        {
+            DEBUG("DEBU: not connected");
+            goto Exit;
+        }
+
+        WiFi.setSleepMode(m_AllowSleep ? WIFI_LIGHT_SLEEP : WIFI_NONE_SLEEP);
+    }
+
+Exit:
+    return;
+}
+
+void
+WIFI::Loop (
+    void ConnectedCallback(void)
+    )
+{
+    switch (m_State)
+    {
+    case WIFI::DISCONNECTED:
+        DEBUG("DEBU: loop disconnected");
+    case WIFI::CONNECTED:
+        if (!IsConnected())
+        {
+            Connect();
+
+            if (m_State == WIFI::CONNECTED)
+            {
+                if (ConnectedCallback != nullptr)
+                {
+                    ConnectedCallback();
+                }
+            }
+        }
+        break;
+
+    case WIFI::CONNECTING:
+        DEBUG("DEBU: loop connecting");
+        WaitForConnection(m_ConnectTimeoutMs);
+
+        if (m_State == WIFI::CONNECTED)
+        {
+            if (ConnectedCallback != nullptr)
+            {
+                DEBUG("DEBU: loop connected!");
+                ConnectedCallback();
+            }
+        }
+        break;
+
+    default:
+        //
+        // SHOULD NEVER HAPPEN!
+        //
+        Serial.println("ASSERT FAILED");
+
+        for (;;)
+            ;;
+        break;
+    }
 }
